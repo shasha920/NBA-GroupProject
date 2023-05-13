ffc80509f5a0b89f4a473a507b32a7049fd29258
shasha920 <96502556+shasha920@users.noreply.github.com>
2023-05-13 00:26:40 -0700

    Add files via upload

---
+++ Checking Entities Queries.sql
@@ -0,0 +1,33 @@
+SELECT * --1
+from player
+
+select * --2 
+from coach
+
+select * --3
+from owner
+
+select * --4
+from team
+
+select * --5
+from stadium
+
+select * --6
+from season
+
+select * --7
+from conference
+
+select * --8
+from division
+
+
+select * --9
+from player_stats
+
+select * --10
+from games
+
+
+
